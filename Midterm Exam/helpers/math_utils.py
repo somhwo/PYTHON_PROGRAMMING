c8c471f6a4981b31e747640eff43ece688a82d40
somhwo <153707442+somhwo@users.noreply.github.com>
2025-10-23 10:53:28 +0800

    Midterm Exam

---
+++ Midterm Exam/helpers/math_utils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def area(l, w):
+    return l * w
+
+
+# In[ ]:
+
---
+++ Midterm Exam/helpers/string_utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def shout(s):
+    return s.upper()
+
+
+# In[ ]:
+
+
+
+
